011bf667b7604dcb507dcf7983b997d63d4db822
Ali Zulfiqar <codewithfourtix@gmail.com>
2026-01-07 15:34:28 +0500

    Skip LOC to fix hang

--- today.py
+++ today.py
@@ -449,7 +449,7 @@ if __name__ == '__main__':
     formatter('account data', user_time)
     age_data, age_time = perf_counter(daily_readme, datetime.datetime(2005, 7, 25))
     formatter('age calculation', age_time)
-    total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
+    total_loc, loc_time = [0, 0, 0, True], 0
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
     commit_data, commit_time = perf_counter(commit_counter, 7)
     star_data, star_time = perf_counter(graph_repos_stars, 'stars', ['OWNER'])
