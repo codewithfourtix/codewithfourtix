71ec47e3f75cc480649ffec9d1e0df826a0b618f
Ali Zulfiqar <codewithfourtix@gmail.com>
2026-01-09 13:30:57 +0500

    Adjust SVG formatting for better layout
    Updated formatting for SVG output to improve layout.

--- today.py
+++ today.py
@@ -70,16 +70,19 @@ def stars_counter(data):
 def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib_data, follower_data, loc_data):
     tree = etree.parse(filename)
     root = tree.getroot()
-    justify_format(root, 'age_data', age_data, 22)
-    justify_format(root, 'commit_data', commit_data, 17)
-    justify_format(root, 'star_data', star_data, 11)
-    justify_format(root, 'repo_data', repo_data, 4)
-    justify_format(root, 'contrib_data', contrib_data)
-    justify_format(root, 'follower_data', follower_data, 7)
+    
+    justify_format(root, 'age_data', age_data, 45)       # Was 22
+    justify_format(root, 'commit_data', commit_data, 35) # Was 17
+    justify_format(root, 'star_data', star_data, 25)     # Was 11
+    justify_format(root, 'repo_data', repo_data, 20)     # Was 4
+    justify_format(root, 'contrib_data', contrib_data, 20) # Added 20 for contributed repos
+    justify_format(root, 'follower_data', follower_data, 20) # Was 7
+    
     justify_format(root, 'loc_data', loc_data[2], 1)
     justify_format(root, 'loc_add', loc_data[0])
     justify_format(root, 'loc_del', loc_data[1], 1)
     tree.write(filename, encoding='utf-8', xml_declaration=True)
+
 
 def justify_format(root, element_id, new_text, length=0):
     if isinstance(new_text, int):
@@ -195,4 +198,5 @@ if __name__ == '__main__':
 
     print('Total GitHub GraphQL API calls:', '{:>3}'.format(sum(QUERY_COUNT.values())))
     for funct_name, count in QUERY_COUNT.items():
+
         print('{:<28}'.format('   ' + funct_name + ':'), '{:>6}'.format(count))
